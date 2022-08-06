e06cc1c8d634fd54b7c8cd4dfd90c769107a5b13
anhuynh <39059539+anhuynh1998@users.noreply.github.com>
2022-08-07 02:25:00 +0700

    fist commit

---
+++ cat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cat extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Cat;
