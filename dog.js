1204cc959a9a4748749bd95cde81404106a69d5a
anhuynh1998 <huynhxuanan199@gmail.com>
2022-08-07 03:13:39 +0700

    -p

---
+++ dog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+    render() {
+        return (
+            <div>
+                <p></p>
+                
+            </div>
+        );
+    }
+}
+
+export default Dog;
